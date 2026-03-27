8934f8e02a8372101083fa44ee7be6f2aed59b84
VR1206 <143418316+VR1206@users.noreply.github.com>
2026-03-27 21:45:45 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const uri = "mongodb+srv://RoXy:Jakhar9014@cluster0.gnnfkcm.mongodb.net/?appName=Cluster0";
+const uri = "mongodb+srv://RoXy:Jakhar9014@cluster0.xvxwgz6.mongodb.net/?appName=Cluster0";
 const client = new MongoClient(uri);
 
 let db;
